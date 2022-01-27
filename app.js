a4e9627f26ed1f6a9e3f71c2772024e1eb36cf60
Ebrima Sallah <sallahebrima@ymail.com>
2022-01-27 15:24:37 +0000

    fixed a calculator functionality

--- app.js
+++ app.js
@@ -9,38 +9,14 @@ let num1 = 0;
 let num2 = 0;
 let operatorSym;
 
-
-// use parent class if bubbling method is used // otherwise use child if foreach is used // create a cnst to store something using bubbling sometimes
-
-// const calculator = {
-//   operator:"",
-//   firstNumber:0,
-//   SecondNumber:"",
-
-
-// };
-
-
-// const calcArray = [...""]; 
-
-// const firstNumbertop = () => {
-
-//   const num1 = calcArray.map(num=>num).pop();
-
-//   console.log(num1);
-
-// }
-
-
-
 const operatorSymbols = document.querySelectorAll('.operators');
 
 for (symbol of operatorSymbols) {
   symbol.addEventListener('click', (event) => {
     displayTop.innerHTML += event.target.innerHTML;
-
+    displayTop.innerHTML="";
     operatorSym = event.target.innerHTML;
-   if (displayTop != "" && operatorSym) return;
+
     console.log(operatorSym);
   } )
 }
@@ -54,8 +30,11 @@ for (button of buttonsNumber) {
     displayTop.innerHTML += event.target.innerHTML;
    
     if (num1 && operatorSym) {
+
       num2 = displayTop.innerHTML;
+
       console.log(num2);
+      
     } else {
       num1 = displayTop.innerHTML
       console.log(num1);
@@ -80,7 +59,6 @@ const calculate = (num1, num2, operator) => {
   if (operator === "+") {
     return parseInt(num1) + parseInt(num2);
   } else if (operator === "-") {
-    console.log(true )
     return parseInt(num1) - parseInt(num2);
   } else if (operator === "x") {
     return parseInt(num1) * parseInt(num2);
@@ -98,12 +76,10 @@ const calculate = (num1, num2, operator) => {
 
 
 
-
-
 equalOperator.addEventListener('click', (event)=> {
   bottomScreen.innerHTML = calculate(num1, num2, operatorSym);
-  displayTop.innerHTML = clear(num1,num2,operatorSym)
-
+  displayTop.innerHTML = clear(num1, num2, operatorSym)
+  
 })
 
   
@@ -111,13 +87,3 @@ clearButton.addEventListener('click', (event)=> {
   bottomScreen.innerHTML = clear (num1, num2, operatorSym);
   displayTop.innerHTML = clear (num1, num2, operatorSym);
 })
-
-// displayTop.addEventListener('input', (event)=> {
-//   // if(num1 && num2 && operatorSym) {
-//   //   return;
-//   // }
-// //   if(bottomScreen !== " ") {
-// //    num1 = 0; num2 = 0;
-// //   }
-// //   console.log(num1, num2);
-// // })
